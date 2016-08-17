77ae902e3d12d0ede59b75b59f11a12b100cc8d7
Kron <gehrinpa_5@hotmail.com>
2016-08-17 22:41:37 +0100

    Dropdowns updated. Graphics in place and click animation functional.

--- app/src/main/java/com/example/kron/googlemapapp/MainActivity.java
+++ app/src/main/java/com/example/kron/googlemapapp/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.kron.googlemapapp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.net.DhcpInfo;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.ContactsContract;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -21,9 +27,12 @@ import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -51,22 +60,27 @@ import java.util.Set;
 
 public class MainActivity extends Activity {
 
-//    private boolean map = false;
+    //    private boolean map = false;
 //    private static final int ERRORDIALOG = 9001;
 //    private GoogleMap mMap;
 //    private MapView mMapView;
 //    private CameraUpdate update = null;
 //    private MarkerOptions[] marker;
-    private Spinner stateSpinner, countySpinner;
+    private Spinner stateSpinner, countySpinner, distSpinner;
     private String countySelected, stateSelected;
     private Intent myIntent = null;
     private final static String MESSAGE1 = "com.example.kron.googlemapapp.county",
-        MESSAGE2 = "com.example.kron.googlemapapp.state";
+            MESSAGE2 = "com.example.kron.googlemapapp.state";
     //private ArrayAdapter<String> stateSpinnerAdapter;
     Thread m_objThreadClient;
     int port = 3447;
     Socket clientSocket;
     WifiManager wifi;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     /**
      * Everything below until the line is only temporary for the alpha build, it will be replaced by
@@ -108,11 +122,31 @@ public class MainActivity extends Activity {
 
         wifi = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
         //Set up the drop down menu for the user to select the State.
-        stateSpinner = (Spinner)findViewById(R.id.StateSpinner);
+        stateSpinner = (Spinner) findViewById(R.id.StateSpinner);
+
+        //Set the click animation.
+        stateSpinner.setOnTouchListener(new View.OnTouchListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)    //Wont work on older API's
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {     //when touched.
+                        stateSpinner.setBackground(getDrawable(R.drawable.dropdownclicked));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {       //when released
+                        stateSpinner.setBackground(getDrawable(R.drawable.dropdown));
+                        break;
+                    }
+                }
+                return false;                           //For some reason if this is not false the dropdown will stop working entirely.
+            }
+        });
+
         ArrayAdapter<String> stateSpinnerAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_dropdown_item,
                 getResources().getStringArray(R.array.stateList)); //Get the list of states from the
-                                                                   //State.xml document.
+        //State.xml document.
         stateSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         stateSpinner.setAdapter(stateSpinnerAdapter);
         stateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -121,10 +155,37 @@ public class MainActivity extends Activity {
                 onSelectState(parent.getItemAtPosition(position).toString());
                 //This is the method called whenever an item is selected.
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
         //-----------------------------------------------------------
+        distSpinner = (Spinner)findViewById(R.id.DistSpinner);
+        ArrayAdapter<String> distSpinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.distList));
+        distSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        distSpinner.setAdapter(distSpinnerAdapter);
+        distSpinner.setOnTouchListener(new View.OnTouchListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)    //Wont work on older API's
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {     //when touched.
+                        distSpinner.setBackground(getDrawable(R.drawable.dropdownsmallclicked));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {       //when released
+                        distSpinner.setBackground(getDrawable(R.drawable.dropdownsmall));
+                        break;
+                    }
+                }
+                return false;                           //For some reason if this is not false the dropdown will stop working entirely.
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -136,15 +197,33 @@ public class MainActivity extends Activity {
      */
     public void onSelectState(final String str) {
         countySpinner = (Spinner) findViewById(R.id.CountySpinner);
+        //Set click animation.
+        countySpinner.setOnTouchListener(new View.OnTouchListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)    //Wont work on older API's
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {     //when touched.
+                        countySpinner.setBackground(getDrawable(R.drawable.dropdownclicked));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {       //when released
+                        countySpinner.setBackground(getDrawable(R.drawable.dropdown));
+                        break;
+                    }
+                }
+                return false;                           //For some reason if this is not false the dropdown will stop working entirely.
+            }
+        });
         //The 2nd spinner on the start page.
-        String tmpStr = str.replace(" ","");
+        String tmpStr = str.replace(" ", "");
         //This changes the state name into the name of the database containing its counties [e.g.
         //"New York" becomes "NewYork"].
         ArrayAdapter<String> countySpinnerAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_dropdown_item,
                 getResources().getStringArray(getResources().getIdentifier(tmpStr, "array", "com.example.kron.googlemapapp")));
-                //The list used to populate the spinner is defined dynamically using the getIdentifier()
-                //call in the inner parentheses. The tmpStr is the string we created earlier.
+        //The list used to populate the spinner is defined dynamically using the getIdentifier()
+        //call in the inner parentheses. The tmpStr is the string we created earlier.
         countySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         countySpinner.setAdapter(countySpinnerAdapter);
         //Toast.makeText(this,getResources().getIdentifier("Alabama", "array", "com.example.kron.googlemapapp"),Toast.LENGTH_SHORT).show();
@@ -168,21 +247,21 @@ public class MainActivity extends Activity {
      * If we do not we put out a Toast and the user is stuck here until they select a valid combination.
      * Otherwise we enable the button for creating the map and define the onBuildMapButton function
      * as the function to call when the button is clicked.
-     *
+     * <p/>
      * We also enable the update button from here regardless of whether or not we already have values
      * for the county in question. When that button is clicked we create and UpdateDB object with the
      * county and state and call its start function
      */
-    public void onSelectCounty(String county, String state){
+    public void onSelectCounty(String county, String state) {
         DatabaseManager DB = new DatabaseManager(this);
         Set<String> tmpSet = DB.getAccidents(county, state);
         //List of accidents.
         countySelected = county;
         stateSelected = state;
         //Set global values.
-        if(tmpSet!=null){
+        if (tmpSet != null) {
             Button makeMap;
-            makeMap = (Button)findViewById(R.id.MakeMapButton);
+            makeMap = (Button) findViewById(R.id.MakeMapButton);
             makeMap.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -190,13 +269,12 @@ public class MainActivity extends Activity {
                     //function called when button is clicked.
                 }
             });
-        }
-        else{
+        } else {
             Toast.makeText(this, "Cannot construct map of this county, pick another or press 'Update'.", Toast.LENGTH_SHORT).show();
             //User done goofed!
         }
         Button updateDB;
-        updateDB = (Button)findViewById(R.id.updateButton);
+        updateDB = (Button) findViewById(R.id.updateButton);
         updateDB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -204,7 +282,7 @@ public class MainActivity extends Activity {
             }
         });
         Button deleteDB;
-        deleteDB = (Button)findViewById(R.id.deleteButton);
+        deleteDB = (Button) findViewById(R.id.deleteButton);
         deleteDB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -218,7 +296,7 @@ public class MainActivity extends Activity {
      * countySelected and stateSelected values and shipping off to the DisplayMap class as our new
      * main.
      */
-    public void onBuildMapButton(){
+    public void onBuildMapButton() {
         myIntent = new Intent(this, DisplayMap.class);
         myIntent.putExtra(MESSAGE1, countySelected);
         myIntent.putExtra(MESSAGE2, stateSelected);
@@ -226,16 +304,16 @@ public class MainActivity extends Activity {
         startActivity(myIntent);
     }
 
-    public void onDeleteMapButton(){
+    public void onDeleteMapButton() {
         DatabaseManager DB = new DatabaseManager(this);
-        DB.deleteCurrent(stateSelected,countySelected);
-        Toast.makeText(this,"Accident list for "+countySelected+" county deleted.",Toast.LENGTH_SHORT).show();
+        DB.deleteCurrent(stateSelected, countySelected);
+        Toast.makeText(this, "Accident list for " + countySelected + " county deleted.", Toast.LENGTH_SHORT).show();
     }
 
     /**
      * Called when the update button is pressed.
      */
-    public void onUpdateMapButton(){
+    public void onUpdateMapButton() {
         m_objThreadClient = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -250,10 +328,10 @@ public class MainActivity extends Activity {
                     byte inblock[] = new byte[256];
                     DatagramPacket inpacket = new DatagramPacket(inblock, inblock.length);
                     udpPing.receive(inpacket);
-                    String instr = new String(inpacket.getData(), 0 ,inpacket.getLength());
+                    String instr = new String(inpacket.getData(), 0, inpacket.getLength());
                     udpPing.close();
 
-                    if(instr.equals("Server Ack")) {
+                    if (instr.equals("Server Ack")) {
                         clientSocket = new Socket(inpacket.getAddress(), port);
                         clientSocket.setReuseAddress(true);
                         ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
@@ -294,20 +372,60 @@ public class MainActivity extends Activity {
 
     Handler mHandler = new Handler() {
         @Override
-        public void handleMessage(Message msg){
-            toDatabase((Set<String>)msg.obj);
+        public void handleMessage(Message msg) {
+            toDatabase((Set<String>) msg.obj);
         }
     };
 
     /**
      * Called by the mHandler so that the accident list can be passed out of the new thread.
      */
-    public void toDatabase(Set<String> serverMsg){
+    public void toDatabase(Set<String> serverMsg) {
         DatabaseManager DB = new DatabaseManager(this);
         DB.createAccidentDB(serverMsg, countySelected, stateSelected);
-        for(String s : serverMsg){
-            Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+        for (String s : serverMsg) {
+            Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.kron.googlemapapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.kron.googlemapapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
 
