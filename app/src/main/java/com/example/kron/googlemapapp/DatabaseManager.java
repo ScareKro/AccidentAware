41366c17e1258b089c2424632869246ddbdb8685
Kron <gehrinpa_5@hotmail.com>
2016-09-04 21:38:18 +0100

    Changed the DB manager though it may become obsolete eventually

--- app/src/main/java/com/example/kron/googlemapapp/DatabaseManager.java
+++ app/src/main/java/com/example/kron/googlemapapp/DatabaseManager.java
@@ -26,27 +26,44 @@ public class DatabaseManager {
      * class and store it in sharedPrefs using the state and county names with a dash in between as
      * the access key.
      */
-    public void createAccidentDB(Set<String> accidentDB, String county, String state){
+    public void addAccidentToDB(Set<String> accident, String longe, String lat){
         SharedPreferences.Editor editor = dataPref.edit();
-        editor.putStringSet(state+"-"+county, accidentDB);
-        editor.commit();
+        String location = longe+"-"+lat;
+        if(dataPref.getString(location, null) != null){
+            editor.remove(location);
+            editor.commit();
+        }
+        editor.putStringSet(location,accident);
     }
+//    public void createAccidentDB(Set<String> accidentDB, String county, String state){
+//        SharedPreferences.Editor editor = dataPref.edit();
+//        editor.putStringSet(state+"-"+county, accidentDB);
+//        editor.commit();
+//    }
 
     /**
      * After selecing the county and state in the drop down menus the two string values are sent here
      * This is where we reference the sharedpreferences to acquire the actual list of accidents from
      * the county.
      */
-    public Set<String> getAccidents(String county, String state){
-        return dataPref.getStringSet(state+"-"+county, null);
+    public Set<String> getAccident(String longe, String lat){
+        return dataPref.getStringSet(longe+"-"+lat,null);
     }
+//    public Set<String> getAccidents(String county, String state){
+//        return dataPref.getStringSet(state+"-"+county, null);
+//    }
 
     /**
      * Function to delete certain key value pairs in the sharedpreferences.
      */
-    public void deleteCurrent(String county, String state){
+    public void clearPrefs(){
         SharedPreferences.Editor editor = dataPref.edit();
-        editor.remove(state+"-"+county);
+        editor.clear();
         editor.commit();
     }
+//    public void deleteCurrent(String county, String state){
+//        SharedPreferences.Editor editor = dataPref.edit();
+//        editor.remove(state+"-"+county);
+//        editor.commit();
+//    }
 }
